c03c6a4adaac606a3b50e1c4aae48445b2db617b
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-27 23:40:50 +0100

    Exercise 8:1 add each_with_index to include number in list of students

--- directory.rb
+++ directory.rb
@@ -11,18 +11,17 @@ def input_students
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
-  students
+    students
 end
-
 
 def print_header
   puts "The students of Villains Academy"
-  puts "-------------"
+  puts "--------------------------------"
 end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
