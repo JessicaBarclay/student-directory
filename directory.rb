f16b8dc4973efbe67f6d619861e884cc9a4a6ee0
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-28 21:17:28 +0100

    Completed 8.5 adding hobbies, country of birth, height hashes to the students array. Refactored in general

--- directory.rb
+++ directory.rb
@@ -1,29 +1,32 @@
 def input_students
+  students = []
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-    students = []
-    name = gets.chomp.capitalize
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
+  puts "To finish, just hit return twice" ; name = gets.chomp.capitalize
+  puts "Any hobbies?" ; hobbies = gets.chomp.capitalize
+  puts "Country of birth?" ; country = gets.chomp.capitalize
+  puts "Height in cm?" ; height = gets.chomp
+    while !name.empty? do
+      students << {name: name, cohort: :november, hobbies: hobbies, country: country, height: height}
+      puts "Now we have #{students.count} students, add another?" ; name = gets.chomp
+      puts "Any hobbies?" ; hobbies = gets.chomp.capitalize
+      puts "Country of birth?" ; country = gets.chomp.capitalize
+      puts "Height in cm?" ; height = gets.chomp
+    end
   students
 end
-
 def print_header
   puts "The students of Villains Academy"
-  puts "--------------------------------"
+  puts "________________________________"
 end
-
 def print(students)
-  students.each_with_index do |student, index|
-    # if student[:name][0] == "A" && student[:name].length < 12
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    # end
+  total = students.count
+  i = 0
+    while total > 0
+    puts "Name: #{students[i][:name]} Cohort: #{students[i][:cohort]} Hobbies: #{students[i][:hobbies]} Country: #{students[i][:country]} Height: #{students[i][:height]}"
+    total -= 1
+    i += 1
   end
 end
-
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
