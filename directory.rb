81dba33431c1cb8b33e67f35981f5edf09024b44
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-29 18:51:57 +0100

    Completed 8.7 : Added typo statement for user to check the students name is spelt correctly before commiting the students information. Converted input strings to symbols in students array

--- directory.rb
+++ directory.rb
@@ -2,22 +2,24 @@ def input_students
   students = []
   puts "Please enter the students information".center(50)
   puts  "To finish, just hit return twice".center(50)
-  puts
-  puts "Students name?".center(50); name = gets.chomp.capitalize
+  puts # format purposes
+  puts "Students name?".center(50); name = gets.chomp.capitalize.to_sym
     while !name.empty? do
-      puts "Students cohort?".center(50) ; cohort = gets.chomp.capitalize
-      if cohort.empty? ; cohort = "April" end
-      puts "Any hobbies?".center(50) ; hobbies = gets.chomp.capitalize
-      puts "Country of birth?".center(50) ; country = gets.chomp.upcase
-      puts "Height in cm?".center(50) ; height = gets.chomp
+      puts "Students cohort?".center(50) ; cohort = gets.chomp.capitalize.to_sym
+      if cohort.empty? ; cohort = "April" end # default value for cohort
+      puts "Any hobbies?".center(50) ; hobbies = gets.chomp.capitalize.to_sym
+      puts "Country of birth?".center(50) ; country = gets.chomp.upcase.to_sym
+      puts "Height in cm?".center(50) ; height = gets.chomp.to_sym
       students << {name: name, cohort: cohort, hobbies: hobbies, country: country, height: height}
-      puts "Now we have #{students.count} students, add another?".center(50) ; name = gets.chomp
+      puts "Confirm name: #{name} by typing yes or no" ; typo = gets.chomp.downcase # chance for user to check/change name
+      if typo == "no" ; puts "Students name?".center(50); name = gets.chomp.capitalize.to_sym end
+      puts "Now we have #{students.count} students, add another?".center(50) ; name = gets.chomp.to_sym
     end
   students
 end
 def print_header
-  puts "The students of Villains Academy".center(50)
-  puts "__________________________________________________"
+  puts "The Students of Villains Academy".center(50)
+  puts ("_"*50)
 end
 def print(students)
   total = students.count
@@ -27,7 +29,7 @@ def print(students)
     puts "Hobbies: #{students[i][:hobbies]}"
     puts "Country: #{students[i][:country]}"
     puts "Height: #{students[i][:height]}"
-    puts "__________________________________________________"
+    puts ("_"*50)
     total -= 1
     i += 1
   end
