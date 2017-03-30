008fc01e88559d4d6a3727caeae67ad9695db465
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-30 16:02:50 +0100

    Completed 12.0 Load data from file

--- directory.rb
+++ directory.rb
@@ -25,6 +25,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 def show_students
@@ -40,6 +41,9 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
+    show_students
   when "9"
     exit
   else
@@ -52,7 +56,6 @@ def print_header
 end
 def print_students_list
   if @students.empty? || @students == nil
-    puts ""
     puts "No students on file\n".center(50)
     else list_by_month = @students.group_by {|input| input[:cohort]}
       puts "Students listed by cohort:".center(50)
@@ -83,6 +86,14 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort}
   end
   file.close
 end
