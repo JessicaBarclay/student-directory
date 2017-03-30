ef76749522e22b24064808c12aeedef8c505bbde
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-30 12:19:28 +0100

    Completed 8.12 - Program will not print in list format if students array is empty - default message will be output instead. Some reformatting in general

--- directory.rb
+++ directory.rb
@@ -7,13 +7,13 @@ def input_students
   puts "Students name?".center(50); name = gets.chop.capitalize.to_sym
     while !name.empty? do
       puts "Cohort?".center(50)
-      cohort = gets.chop.capitalize.to_sym
-      if cohort.empty? ; cohort = "N/A" end
-      puts "Country of birth?".center(50) ; country = gets.chop.to_sym
-      students << {name: name, cohort: cohort, country: country}
-      puts "Confirm name: '#{name}' by typing yes or no" ; typo = gets.chop.downcase
-      if typo == "no" ; puts "Students name?".center(50); name = gets.chop.capitalize.to_sym end
-      puts "Now we have #{students.count} students".center(50)
+        cohort = gets.chop.capitalize.to_sym
+          if cohort.empty? ; cohort = "Cohort tbc" end
+          puts "Country of birth?".center(50) ; country = gets.chop.to_sym
+          students << {name: name, cohort: cohort, country: country}
+          puts "Confirm name: '#{name}' by typing yes or no" ; typo = gets.chop.downcase
+          if typo == "no" ; puts "Students name?".center(50); name = gets.chop.capitalize.to_sym end
+        puts "Now we have #{students.count} students".center(50)
       puts "Hit return or add another name:".center(50) ; name = gets.chop.to_sym
     end
   students
@@ -23,21 +23,29 @@ def print_header
   puts ("_"*50)
 end
 def print(students)
-  list_by_month = students.group_by {|input| input[:cohort]}
-    puts "Students listed by cohort:".center(50)
-      list_by_month.map do |key, value|
-        puts "#{key}"
-        for index in 0..value.size-1 do
-      puts "#{index+1}. #{value[index][:name]}, #{value[index][:country]}"
+  if students.empty? || students == nil
+    puts ""
+      puts "No students on file\n".center(50)
+        else list_by_month = students.group_by {|input| input[:cohort]}
+          puts "Students listed by cohort:".center(50)
+            list_by_month.map do |key, value|
+            puts "#{key}"
+          for index in 0..value.size-1 do
+        puts "#{index+1}. #{value[index][:name]}, #{value[index][:country]}"
+      end
     end
   end
 end
 def print_footer(students)
-  if students.count == 1
-    puts "Overall, we have a great student"
-    puts ("_"*50)
-  else
-    puts "Overall, we have #{students.count} great students".center(50)
+  if students.count == 0
+    return
+      elsif students.count == 1
+        puts ""
+          puts "We have a great student".center(50)
+            puts ("_"*50)
+          else
+        puts ""
+      puts "Overall, we have #{students.count} great students".center(50)
     puts ("_"*50)
   end
 end
