96530b09c14a9030489d18d6c3aa2bcbd7591723
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-30 18:57:32 +0100

    Partially completed 14.3, will revisit as development continues

--- directory.rb
+++ directory.rb
@@ -1,10 +1,8 @@
 @students = []
 
 def print_menu_options
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "1. Input the students", "2. Show the students"
+  puts "3. Save the list to students.csv", "4. Load the list from students.csv"
   puts "9. Exit"
 end
 def interactive_menu
@@ -15,33 +13,26 @@ def interactive_menu
 end
 def process(selection)
   case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  else
-    puts "Try again"
+  when "1" ; input_students
+  when "2" ; show_students
+  when "3" ; save_students
+  when "4" ; load_students
+  when "9" ; exit
+  else ; puts "Try again"
   end
 end
-def data_list
+def array_list
   @students << {name: @name, cohort: @cohort}
 end
 def input_students
-  puts ("_"*50)
-  puts "Please enter the students information".center(50)
+  puts "_"*50, "Please enter the students information".center(50)
   puts  "To finish, just hit return twice".center(50)
   @name = STDIN.gets.chop.capitalize.to_sym
     while !@name.empty? do
       puts "Cohort?".center(50)
       @cohort = STDIN.gets.chop.capitalize.to_sym
       if @cohort.empty? ; @cohort = "Cohort tbc" end
-      data_list
+      array_list
       puts "Now we have #{@students.count} students".center(50)
       puts "Hit return or add another name:".center(50)
       @name = STDIN.gets.chop.to_sym
@@ -53,30 +44,23 @@ def show_students
   print_footer
 end
 def print_header
-  puts "The Students of Villains Academy".center(50)
-  puts ("_"*50)
+  puts "The Students of Villains Academy".center(50), "_"*50
 end
 def print_students_list
   @students.each_with_index do |student, i|
-    puts "#{i + 1}.#{student[:name]}, #{student[:cohort]}"
+    puts "#{i+1}.#{student[:name]}, #{student[:cohort]}"
   end
 end
 def print_footer
-  if @students.count == 0
-    return
+  if @students.count == 0 ; return
   elsif @students.count == 1
-    puts ""
-    puts "We have a great student".center(50)
-    puts ("_"*50)
+    puts "\nWe have a great student".center(50), "_"*50
   else
-    puts ""
-    puts "Overall, we have #{@students.count} great students".center(50)
-    puts ("_"*50)
+    puts "\nWe have #{@students.count} great students".center(50), "_"*50
   end
 end
 def save_students
-  puts "Data saved".center(50)
-  puts
+  puts "Data saved".center(50), ""
   file = File.open("students.csv", "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
@@ -86,12 +70,11 @@ def save_students
   file.close
 end
 def load_students(filename = "students.csv")
-  puts "Data loaded, hit 2 to view".center(50)
-  puts
+  puts "Data loaded, hit 2 to view".center(50), ""
   file = File.open(filename, "r")
   file.readlines.each do |line|
-  @name, @cohort = line.chomp.split(",")
-  data_list
+    @name, @cohort = line.chomp.split(",")
+    array_list
   end
   file.close
 end
