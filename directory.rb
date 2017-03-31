ebfece0c9f5ca86492929377237cfcfa3c5d206b
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-31 18:20:06 +0100

    Completed 14.6 Refactored load/save methods to use block

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ def print_menu_options
   puts "4. Save file" , "5. Load file", "9. Exit"
 end
 def interactive_menu
-  load_on_startup
+  load_startup_option
   loop do
     print_menu_options
     process(STDIN.gets.chomp)
@@ -48,8 +48,8 @@ def print_students_list
 end
 def print_footer
   if @students.count == 0 ; return
-  elsif @students.count == 1 ; puts "We have a great student!".center(50)
-  else puts "We have #{@students.count} great students!".center(50)
+    elsif @students.count == 1 ; puts "We have a great student!".center(50)
+    else puts "We have #{@students.count} great students!".center(50)
   end
 end
 def print_by_cohort
@@ -64,43 +64,46 @@ def print_by_cohort
 end
 def save_to_file
   puts "Save file as:".center(50)
-  file_name = STDIN.gets.chomp
-  file = File.open(file_name, "w")
+  filename = STDIN.gets.chomp
+  file = File.open(filename, "w") do |file|
     @students.each do |student|
       student_data = [student[:name], student[:cohort]]
-      csv_line = student_data.join(",")
-      file.puts csv_line
+        csv_line = student_data.join(",")
+        file.puts csv_line
+      end
     end
-  file.close
+  puts "Saved: #{filename}".center(50)
 end
 def load_file
   puts "Load file:".center(50), ""
-  file = File.open(STDIN.gets.chomp, "r")
+  filename = STDIN.gets.chomp
+  file = File.open(filename, "r") do |file|
     file.readlines.each do |line|
       @name, @cohort = line.chomp.split(",")
       student_list_array
     end
-  file.close
+  end
+  puts "Loaded: #{filename}".center(50)
 end
-def load_students_default(filename="students.csv")
+def load_default(filename="students.csv")
   puts "","'#{filename}' loaded by default".center(50), ""
-  file = File.open(filename, "r")
+  file = File.open(filename, "r") do |file|
     file.readlines.each do |line|
       @name, @cohort = line.chomp.split(",")
       student_list_array
     end
-  file.close
+  end
 end
-def load_on_startup
+def load_startup_option
   filename = ARGV.first
-  if filename.nil? || filename.empty? ; load_students_default
+  if filename.nil? || filename.empty? ; load_default
   else puts "","'#{filename}' loaded".center(50), ""
-    file = File.open(filename, "r")
+    file = File.open(filename, "r") do |file|
       file.readlines.each do |line|
         @name, @cohort = line.chomp.split(",")
         student_list_array
       end
-    file.close
+    end
   end
 end
 
