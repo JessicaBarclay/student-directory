a646880f68c17e5a4db135a621d5b9728bcca6d4
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-31 11:46:29 +0100

    Completed 14.2 load students.csv dy default if no arg/file is given on startup.

--- directory.rb
+++ directory.rb
@@ -2,10 +2,10 @@
 
 def print_menu_options
   puts "1. Input the students", "2. Show the students"
-  puts "3. Save the list to students.csv", "4. Load the list from students.csv"
-  puts "9. Exit"
+  puts "3. Save to students.csv", "4. Load from students.csv", "9. Exit"
 end
 def interactive_menu
+  load_on_startup
   loop do
     print_menu_options
     process(STDIN.gets.chomp)
@@ -13,13 +13,9 @@ def interactive_menu
 end
 def process(selection)
   case selection
-  when "1" ; input_students
-  when "2" ; show_students
-  when "3" ; save_students
-  when "4" ; load_students
-  when "9" ; exit
-  else ; puts "Try again"
-  end
+  when "1" ; input_students ; when "2" ; show_students
+  when "3" ; save_to_students_file ; when "4" ; load_students_file
+  when "9" ; exit ; else "Try again" end
 end
 def array_list
   @students << {name: @name, cohort: @cohort}
@@ -39,12 +35,10 @@ def input_students
     end
 end
 def show_students
-  print_header
-  print_students_list
-  print_footer
+  print_header ; print_students_list ; print_footer
 end
 def print_header
-  puts "The Students of Villains Academy".center(50), "_"*50
+  puts "The Students of Villains Academy".center(50), "*"*50
 end
 def print_students_list
   @students.each_with_index do |student, i|
@@ -54,12 +48,12 @@ end
 def print_footer
   if @students.count == 0 ; return
   elsif @students.count == 1
-    puts "\nWe have a great student".center(50), "_"*50
+    puts "","We have a great student!".center(50)
   else
-    puts "\nWe have #{@students.count} great students".center(50), "_"*50
+    puts "","We have #{@students.count} great students!".center(50)
   end
 end
-def save_students
+def save_to_students
   puts "Data saved".center(50), ""
   file = File.open("students.csv", "w")
   @students.each do |student|
@@ -69,7 +63,7 @@ def save_students
   end
   file.close
 end
-def load_students(filename = "students.csv")
+def load_students_file(filename="students.csv")
   puts "Data loaded, hit 2 to view".center(50), ""
   file = File.open(filename, "r")
   file.readlines.each do |line|
@@ -78,17 +72,19 @@ def load_students(filename = "students.csv")
   end
   file.close
 end
-def default_load_file
-  filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
-    load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
-  else
-    puts "Sorry, #{filename} doesn't exist"
-  end
+def load_on_startup
+  if ARGV.empty? ; load_students_file end
 end
 
-
-default_load_file
 interactive_menu
+
+# def default_load_file
+#   filename = ARGV.first
+#   return if filename.nil?
+#   if File.exists?(filename)
+#     load_students_file(filename)
+#     puts "Loaded #{@students.count} from #{filename}"
+#   else
+#     puts "Sorry, #{filename} doesn't exist"
+#   end
+# end
